4766d5cf583165273d81a3370fa2725b52e62551
yahayakenny <y.hkehinde@yahoo.com>
2020-07-20 15:56:56 +0100

    initial commit

---
+++ date_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DateAppConfig(AppConfig):
+    name = 'date_app'
---
+++ date_app/urls.py
@@ -0,0 +1,6 @@
+from date_app import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name = 'index' )
+]
